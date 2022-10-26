d68212fcc1a0d6f6598e6a9ff5ebf2fc2aab50ab
euclidespaim <euclidespaim@gmail.com>
2022-10-26 10:43:30 -0300

    Componente tipado

--- stories/Ds.Botao.stories.tsx
+++ stories/Ds.Botao.stories.tsx
@@ -1,6 +1,6 @@
 import { ComponentMeta, ComponentStory } from '@storybook/react';
 import React from 'react';
-import { DsBotao } from '../src/components/DsBotao';
+import { DsBotao, DsBotaoProps } from '../src/components/DsBotao';
 
 export default {
   title: 'Componentes/DsBotao',
@@ -8,6 +8,18 @@ export default {
 
 } as ComponentMeta<typeof DsBotao>
 
-const Template: ComponentStory<typeof DsBotao> = () => <DsBotao />
+const Template: ComponentStory<typeof DsBotao> = (args) => <DsBotao {...args}/>
 
 export const Primario = Template.bind({})
+
+Primario.args = {
+  texto: 'Botão Primário',
+  tipo: 'primario'
+} as DsBotaoProps
+
+export const Secundario = Template.bind({})
+
+Secundario.args = {
+  texto: 'Botão Secundário',
+  tipo: 'primario'
+} as DsBotaoProps
